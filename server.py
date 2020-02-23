08f6328a3b2d7a44b28c6f7e33bd20a3f3d552a0
Rodolfo Araujo <rodoufu@gmail.com>
2020-02-23 12:12:03 -0300

    Using WSGIServer from gevent.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import connexion
 import os
+from gevent.pywsgi import WSGIServer
 
 app = connexion.App(__name__, specification_dir='./')
 app.add_api('swagger.yml')
@@ -7,8 +8,10 @@ app.add_api('swagger.yml')
 if __name__ == '__main__':
 	app_host = os.environ['APP_HOST'] if 'APP_HOST' in os.environ else '0.0.0.0'
 	app_port = int(os.environ['APP_PORT']) if 'APP_PORT' in os.environ else 5000
-	app_debug = bool(os.environ['APP_DEBUG']) if 'APP_DEBUG' in os.environ else True
 
-	print(f"Using host: {app_host}, port: {app_port}, debug: {app_debug}")
+	print(f"Using host: {app_host}, port: {app_port}")
 
-	app.run(host=app_host, port=app_port, debug=app_debug)
+	http_server = WSGIServer((app_host, app_port), app)
+	http_server.serve_forever()
+else:
+	app = app.app
